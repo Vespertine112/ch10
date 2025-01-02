8f64550d396761f9b444be1eee9205b0943afdab
hillbgh@gmail.com <hillbgh@gmail.com>
2025-01-02 12:01:15 -0500

    Added documentation

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
+// Example package which exposes an addition function
 package ch10
 
 
 // Add
 // Sums two ints together and returns an int
+//
+// Refer to [Addition] for more information
+//
+// [Addition]: https://mathisfun.com/numbers/addition.html
 func Add(a,b int) int {
 	 return a + b
 }
